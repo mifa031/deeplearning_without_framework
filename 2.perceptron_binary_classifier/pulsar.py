84f8e6c4430c5b84d07ef6ee468192f6d26881ac
mifa031 <scifimov@naver.com>
2019-08-27 20:58:27 +0900

    Add files via upload

---
+++ 2.perceptron_binary_classifier/pulsar.py
@@ -0,0 +1,141 @@
+import numpy as np
+import csv
+import time
+
+np.random.seed(1234)
+def randomize(): np.random.seed(time.time())
+
+RND_MEAN = 0
+RND_STD = 0.0030
+
+LEARNING_RATE = 0.001
+
+def pulsar_exec(epoch_count=10, mb_size=10, report=1):
+    load_pulsar_dataset()
+    init_model()
+    train_and_test(epoch_count, mb_size, report)
+
+def load_pulsar_dataset():
+    with open('./pulsar_stars.csv') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader, None)
+        rows = []
+        for row in csvreader:
+            rows.append(row)
+
+    global data, input_cnt, output_cnt
+    input_cnt, output_cnt = 8, 1
+    data = np.asarray(rows, dtype='float32')
+
+def forward_postproc(output, y):
+    entropy = sigmoid_cross_entropy_with_logits(y, output)
+    loss = np.mean(entropy)
+    return loss, [y, output, entropy]
+
+def backprop_postproc(G_loss, aux):
+    y, output, entropy = aux
+
+    g_loss_entropy = 1.0 / np.prod(entropy.shape)
+    g_entropy_output = sigmoid_cross_entropy_with_logits_derv(y, output)
+
+    G_entropy = g_loss_entropy * G_loss
+    G_output = g_entropy_output * G_entropy
+
+    return G_output
+    
+
+def eval_accuracy(output, y):
+    estimate = np.greater(output, 0)
+    answer = np.greater(y, 0.5)
+    correct = np.equal(estimate, answer)
+
+    return np.mean(correct)
+
+def relu(x):
+    return np.maximum(x,0)
+def sigmoid(x):
+    return np.exp(-relu(-x)) / (1.0 + np.exp(-np.abs(x)))
+def sigmoid_derv(x, y):
+    return y * (1-y)
+def sigmoid_cross_entropy_with_logits(z, x):
+    return relu(x) - x*z + np.log(1+np.exp(-np.abs(x)))
+def sigmoid_cross_entropy_with_logits_derv(z, x):
+    return -z + sigmoid(x)
+
+def init_model(): #reuse
+    global weight, bias, input_cnt, output_cnt
+    weight = np.random.normal(RND_MEAN, RND_STD, [input_cnt, output_cnt])
+    bias = np.zeros([output_cnt])
+
+def train_and_test(epoch_cnt, mb_size, report): #reuse
+    step_count = arrange_data(mb_size)
+    test_x, test_y = get_test_data()
+
+    for epoch in range(epoch_cnt):
+        losses, accs = [], []
+
+        for n in range(step_count):
+            train_x, train_y = get_train_data(mb_size, n)
+            loss, acc = run_train(train_x, train_y)
+            losses.append(loss)
+            accs.append(acc)
+
+        if report > 0 and (epoch+1) % report == 0:
+            acc = run_test(test_x, test_y)
+            print('Epoch {}: loss={:.3f}, accuracy={:.3f}/{:.3f}'\
+                  .format(epoch+1, np.mean(losses), np.mean(accs), acc))
+    final_acc = run_test(test_x, test_y)
+    print('\nFinal Test: final accuracy = {:.3f}'.format(final_acc))
+
+def arrange_data(mb_size): #reuse
+    global data, shuffle_map, test_begin_idx
+    shuffle_map = np.arange(data.shape[0])
+    np.random.shuffle(shuffle_map)
+    step_count = int(data.shape[0] * 0.8) // mb_size
+    test_begin_idx = step_count * mb_size
+    return step_count
+
+def get_test_data(): #reuse
+    global data, shuffle_map, test_begin_idx, output_cnt
+    test_data = data[shuffle_map[test_begin_idx:]]
+    return test_data[:,:-output_cnt], test_data[:, -output_cnt:]
+
+def get_train_data(mb_size, nth): #reuse
+    global data, shuffle_map, test_begin_idx, output_cnt
+    if nth == 0:
+        np.random.shuffle(shuffle_map[:test_begin_idx])
+    train_data = data[shuffle_map[mb_size*nth : mb_size*(nth+1)]]
+    return train_data[:,:-output_cnt], train_data[:,-output_cnt:]
+
+def run_train(x, y): #reuse
+    output, aux_nn = forward_neuralnet(x)
+    loss, aux_pp = forward_postproc(output, y)
+    accuracy = eval_accuracy(output, y)
+
+    G_loss = 1.0
+    G_output = backprop_postproc(G_loss, aux_pp)
+    backprop_neuralnet(G_output, aux_nn)
+
+    return loss, accuracy
+
+def run_test(x, y): #reuse
+    output, _ = forward_neuralnet(x)
+    accuracy = eval_accuracy(output, y)
+    return accuracy
+
+def forward_neuralnet(x): #reeuse
+    global weight, bias
+    output = np.matmul(x, weight) + bias
+    return output, x
+
+def backprop_neuralnet(G_output, x): #reuse
+    global weight, bias
+    g_output_w = x.transpose()
+
+    G_w = np.matmul(g_output_w, G_output)
+    G_b = np.sum(G_output, axis=0)
+
+    weight -= LEARNING_RATE * G_w
+    bias -= LEARNING_RATE * G_b
+
+pulsar_exec()
